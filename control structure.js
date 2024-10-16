508fbf291dd495b0195aac04debe73fe81a17e1b
OTTO AMOS ALVIN <amosalvinotto@gmail.com>
2024-10-16 15:48:03 +0300

    Add files via upload

---
+++ control structure.js
@@ -0,0 +1,14 @@
+let score = 80;
+switch (score){
+    case 100:
+        console.log('A grade');
+        break;
+     case 90:
+        console.log('A Grade');
+            break;
+     case 76:
+        console.log('B Grade');
+        break;
+            default:
+        console.log('fail')
+}
